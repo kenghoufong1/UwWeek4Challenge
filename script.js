e33c95f5970fd306bdc8cbc05cc831d378fff593
kenghoufong1 <kenghoufong1@gmail.com>
2022-12-05 05:12:21 -0800

    made somemore changes to the css and js file

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 var startbutton = document.getElementById("startbutton");
 var timeEl = document.getElementById("time");
 var fullpage = document.querySelector("body");
-var quizquestion = document.getElementById("question");
 var timerhs = document.querySelector(".firstpage");
 var questionEl = document.getElementById("question");
-var answerList =document.getElementById("answers");
+var answerList = document.getElementById("answers");
+var firstcontent = document.querySelector(".middle");
 
 
-startbutton.addEventListener("click" , startquiz);
+startbutton.addEventListener("click", startquiz);
 
 //created a function that counts down the time and appears game over when time runs out
 var timeleft = 90;
-function countdown(){
-    var timerinterval = setInterval(function(){
-        timeleft --;
-        timeEl.textContent = timeleft +" sec until Gameover";
-        if(timeleft === 0){
+function countdown() {
+    var timerinterval = setInterval(function () {
+        timeleft--;
+        timeEl.textContent = timeleft + " sec until Gameover";
+        if (timeleft === 0) {
             clearInterval(timerinterval);
-            fullpage.innerHTML = " ";
-            
-            
+            Gameover();
+
+
         }
-    },1000);
+    }, 1000);
 }
 
 // countdown();
@@ -29,39 +29,54 @@ function countdown(){
 
 
 const quiz = [{
-    question: "what is HTML?",
-    answers: ["HyperText Markup Language","1","2","3"],
+    question: "What is HTML?",
+    answers: ["HyperText Markup Language", "A website", "An international language", "An App"],
     correctIndex: 0
 },
 {
-    question:"what is CSS?",
-    answers: ["css","2","5","20"],
-    correctindex: 2
+    question: "what is CSS?",
+    answers: ["A Video Game", "Cascading Style Sheets", "Computer Science Search", "An App"],
+    correctindex: 1
 },
 {
-    question:"what is JS?",
-    answers: ["JavaScript","200","2050","500"],
+    question: "What is JS?",
+    answers: ["JavaScript", "An App", "A Computer ", "A Brand"],
     correctindex: 0
 }];
 
-function startquiz(){
-    timerhs.setAttribute("style","display: inline");
+function startquiz() {
+    timerhs.setAttribute("style", "display: inline");
+    firstcontent.setAttribute("style", "display:none");
+    appearanswer();
+    countdown();
 }
 
 questionIndex = 0;
 
-function displayquestion(){
-    if(questionIndex > quiz.length -1){
-        // endquiz
-    }
-    
+function Gameover(){
+    fullpage.innerHTML = " ";
+    let newpage = document.createElement("h1");
+    newpage.textContent = "GameOver Your Final Score is " + timeleft;
+    fullpage.appendChild(newpage);
+
 }
 
- questionEl.textContent = quiz[questionIndex].question;
- for (let i = 0; i < quiz.length + 1; i++) {
-    let answerEl = document.createElement("li");
-    answerEl.textContent = quiz[questionIndex].answers[i];
-    answerList.appendChild(answerEl);
- }
+function appearanswer() {
+    if (questionIndex > quiz.length - 1) {
+        Gameover();
+    }
+    questionEl.textContent = quiz[questionIndex].question;
+    for (let i = 0; i < quiz.length + 1; i++) {
+        let answerEl = document.createElement("li");
+        answerEl.textContent = quiz[questionIndex].answers[i];
+        answerList.appendChild(answerEl);
+        answerEl.addEventListener("click", function () {
+            questionIndex++;
+            answerList.innerHTML = "";
+            appearanswer();
+        })
 
+    }
+
+}
 
